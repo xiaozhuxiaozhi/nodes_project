74d2bb91b56b70dc06c13bda89794ab014afb9e2
朱自伟 <2085188234@qq.comgit config --global user.email 2085188234@qq.com>
2018-07-08 15:59:53 +0800

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var filter =require('gulp-filter');
-var browserSync =require('browser-sync');
-var imagemin =require('gulp-imagemin');
-var reload =browserSync.reload;
+var filter = require('gulp-filter');
+var browserSync = require('browser-sync');
+var imagemin = require('gulp-imagemin');
+var reload = browserSync.reload;
 gulp.task('sass', function() {
   //将你的默认的任务代码放在这
   // return sass('public/sass/*.scss',{style:'compressed'})
@@ -13,38 +13,38 @@ gulp.task('sass', function() {
   // }));
 
   return gulp.src('public/sass/*.scss')
-         .pipe(sass({style:'compressed'}))
-         .pipe(gulp.dest('dist/css'))
-         .pipe(filter('**/*.css'))
-         .pipe(reload({stream:true}));
+    .pipe(sass({ style: 'compressed' }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(filter('**/*.css'))
+    .pipe(reload({ stream: true }));
 });
 
-gulp.task('scripts',function(){
-	return gulp.src('public/js/*.js')
-	.pipe(gulp.dest('dist/js'))
-	.pipe(reload({stream:true}));
+gulp.task('scripts', function() {
+  return gulp.src('public/js/*.js')
+    .pipe(gulp.dest('dist/js'))
+    .pipe(reload({ stream: true }));
 });
 
-gulp.task('images',function(){
-	return gulp.src('public/images/**/*')
-	.pipe(imagemin({optimizationLevel:3, progressive:true,interlaced:true}))
-	.pipe(gulp.dest('dist/src/img'));
+gulp.task('images', function() {
+  return gulp.src('public/images/**/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/src/img'));
 });
 
-gulp.task('server',['sass','scripts','images'],function(){
-	browserSync({
-		server:{
-			baseDir:'./'
-		},
-		port:8000
-	});
-	gulp.watch(['*.html','public/js/*.js'],{
-		cwd:'./'
-	},reload);
+gulp.task('server', ['sass', 'scripts', 'images'], function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    port: 8000
+  });
+  // gulp.watch(['*.html','public/js/*.js'],{
+  // 	cwd:'./'
+  // },reload);
 
-	gulp.watch('public/sass/*.scss',['sass']);
+  // gulp.watch('public/sass/*.scss',['sass']);
 
-	gulp.watch('public/js/*.js',['scripts']);
+  // gulp.watch('public/js/*.js',['scripts']);
 
-	gulp.watch('public/images/**/*',['images']);
+  // gulp.watch('public/images/**/*',['images']);
 });
